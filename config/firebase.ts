9be45ca08af443590cb0d41feca851b2718df8e1
prrockzed <priyanshuranjan2806@gmail.com>
2023-12-25 14:17:45 +0530

    Added firebase config

---
+++ config/firebase.ts
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+import { FIREBASE_API_KEY } from "./apiKey";
+
+const firebaseapikey = FIREBASE_API_KEY;
+const firebaseConfig = {
+  apiKey: firebaseapikey,
+  authDomain: "prrockzednews.firebaseapp.com",
+  projectId: "prrockzednews",
+  storageBucket: "prrockzednews.appspot.com",
+  messagingSenderId: "383166514439",
+  appId: "1:383166514439:web:39e5944a099f5fbc8a71a2",
+};
+
+const app = initializeApp(firebaseConfig);
